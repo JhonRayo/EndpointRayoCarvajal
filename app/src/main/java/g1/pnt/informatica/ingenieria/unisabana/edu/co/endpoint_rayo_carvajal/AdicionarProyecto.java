b06daa27988e50579a9e70ff61b3ffc3d10b80b7
JhonRayo <38792701+JhonRayo@users.noreply.github.com>
2018-04-27 19:11:22 -0500

    Initial commit

---
+++ app/src/main/java/g1/pnt/informatica/ingenieria/unisabana/edu/co/endpoint_rayo_carvajal/AdicionarProyecto.java
@@ -0,0 +1,13 @@
+package g1.pnt.informatica.ingenieria.unisabana.edu.co.endpoint_rayo_carvajal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdicionarProyecto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_proyecto);
+    }
+}
